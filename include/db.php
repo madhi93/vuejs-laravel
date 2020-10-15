6dd827bd4be2986f30ee9d1c62fea6d6aafa8535
Mathi <madhivanan@live.com>
2020-10-15 12:57:24 +0530

    bug fix

--- include/db.php
+++ include/db.php
@@ -6,6 +6,8 @@ use Google\Cloud\Firestore\FirestoreClient;
 try{
     $database = new FirestoreClient(['projectId' => 'hirelocal-a2988']);
 
+    echo "<script>alert('connection successful')</script>";
+
 }catch(Exception $e)
 {
     echo "<script>alert('You have an error ".$e." Please contact to the developer')</script>";
